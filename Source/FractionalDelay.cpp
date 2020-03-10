6a07b5256f9f375481fb1502120771ca34a2a3a6
hackaudio <hackaudio@gmail.com>
2020-03-10 12:41:48 -0500

    Modified fractional delay class so it can also be used for predelay

--- Source/FractionalDelay.cpp
+++ Source/FractionalDelay.cpp
@@ -23,44 +23,49 @@ FractionalDelay::~FractionalDelay(){
 
 float FractionalDelay::processSample(float x, int channel){
     
-    float lfo;
-    
-    lfo = depth * sin(currentAngle[channel]);
-    
-    currentAngle[channel] += angleChange;
-    if (currentAngle[channel] > 2.f * M_PI){
-        currentAngle[channel] -= 2.f * M_PI;
-    }
-    
-    // Delay Buffer
-    // "delay" can be fraction
-    int d1 = floor(delay+lfo);
-    int d2 = d1 + 1;
-    float g2 = delay + lfo - (float)d1;
-    float g1 = 1.0f - g2;
-    
-    int indexD1 = index[channel] - d1;
-    if (indexD1 < 0){
-        indexD1 += MAX_BUFFER_SIZE;
-    }
-    
-    int indexD2 = index[channel] - d2;
-    if (indexD2 < 0){
-        indexD2 += MAX_BUFFER_SIZE;
-    }
-    
-    float y = g1 * delayBuffer[indexD1][channel] + g2 * delayBuffer[indexD2][channel];
-    
-    delayBuffer[index[channel]][channel] = x;
-    
-    if (index[channel] < MAX_BUFFER_SIZE - 1){
-        index[channel]++;
+    if (delay < 1.f){
+        return x;
     }
     else{
-        index[channel] = 0;
+        float lfo;
+        
+        lfo = depth * sin(currentAngle[channel]);
+        
+        currentAngle[channel] += angleChange;
+        if (currentAngle[channel] > 2.f * M_PI){
+            currentAngle[channel] -= 2.f * M_PI;
+        }
+        
+        // Delay Buffer
+        // "delay" can be fraction
+        int d1 = floor(delay+lfo);
+        int d2 = d1 + 1;
+        float g2 = delay + lfo - (float)d1;
+        float g1 = 1.0f - g2;
+        
+        int indexD1 = index[channel] - d1;
+        if (indexD1 < 0){
+            indexD1 += MAX_BUFFER_SIZE;
+        }
+        
+        int indexD2 = index[channel] - d2;
+        if (indexD2 < 0){
+            indexD2 += MAX_BUFFER_SIZE;
+        }
+        
+        float y = g1 * delayBuffer[indexD1][channel] + g2 * delayBuffer[indexD2][channel];
+        
+        delayBuffer[index[channel]][channel] = x;
+        
+        if (index[channel] < MAX_BUFFER_SIZE - 1){
+            index[channel]++;
+        }
+        else{
+            index[channel] = 0;
+        }
+        
+        return y;
     }
-    
-    return y;
 }
 
 void FractionalDelay::setFs(float Fs){
@@ -71,6 +76,9 @@ void FractionalDelay::setFs(float Fs){
 void FractionalDelay::setDelaySamples(float delay){
     if (delay >= 1.f){
         this->delay = delay;
+    }
+    else{
+        this->delay = 0.f;
     }
 }
 
@@ -83,7 +91,6 @@ void FractionalDelay::setSpeed(float speed){
 }
 
 void FractionalDelay::setDepth(float depth){
-    if (depth >= 1.0f && depth <= 10.0f){
-        this->depth = depth;
-    }
+
+    this->depth = depth;
 }
